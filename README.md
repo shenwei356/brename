47b258ebfab758d4a83423568835a299c0ead2d8
Wei Shen <shenwei356@gmail.com>
2017-04-23 01:50:42 +0800

    rewrite

--- README.md
+++ README.md
@@ -1,44 +1,121 @@
-brename
-=======
+# brename -- a cross-platform command-line tool for batch renaming files/directories
 
-Recursively batch rename files and directories by regular expression.
-
-通过正则表达式递归、批量重命名文件和文件夹。
-
-Install
--------
-
-[Download](https://github.com/shenwei356/brename/releases)
-
-This package is "go-gettable", just:
-
-    go get github.com/shenwei356/brename
-    go install github.com/shenwei356/brename
-
-Usage
------
-    
-    Usage: brename -s <regexp> -r <replacement> [-R] [-D] [path...]
-
-    Options:
-      -D=false: Rename directories
-      -R=false: Recursively rename
-      -r="": Replacement
-      -s="": Regular expression
+[![Built with GoLang](https://img.shields.io/badge/powered_by-go-6362c2.svg?style=flat)](https://golang.org)
+[![Go Report Card](https://goreportcard.com/badge/github.com/shenwei356/brename)](https://goreportcard.com/report/github.com/shenwei356/brename)
+[![Cross-platform](https://img.shields.io/badge/platform-any-ec2eb4.svg?style=flat)](#download)
+[![Latest Version](https://img.shields.io/github/release/shenwei356/brename.svg?style=flat?maxAge=86400)](https://github.com/shenwei356/brename/releases)
+[![Github Releases](https://img.shields.io/github/downloads/shenwei356/brename/latest/total.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases)
 
 
-Example
--------
-    
-  1. a.jpeg -> a.jpg
+## Table of Contents
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
 
-    brename -s '\.jpeg$' -r '.jpg'
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Examples](#examples)
+- [Contact](#contact)
+- [License](#license)
 
-  2. ab.png -> abab.png
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
-    brename -s '([ab]+)' -r '$1$1'
 
-    
-Copyright (c) 2014, Wei Shen (shenwei356@gmail.com)
+## Features
+
+## Installation
+
+`brename` is implemented in [Go](https://golang.org/) programming language,
+ executable binary files **for most popular operating systems** are freely available
+  in [release](https://github.com/shenwei356/brename/releases) page.
+
+#### Method 1: Download binaries
+
+[brename v0.2.0](https://github.com/shenwei356/brename/releases/tag/v0.2.0)
+[![Github Releases (by Release)](https://img.shields.io/github/downloads/shenwei356/brename/v0.2.0/total.svg)](https://github.com/shenwei356/brename/releases/tag/v0.1.7)
+
+
+OS     |Arch      |File, (mirror为中国用户下载镜像链接)                                                                                                                                                                         |Download Count
+:------|:---------|:--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Linux  |32-bit    |[brename_linux_386.tar.gz](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_linux_386.tar.gz), ([mirror](http://app.shenwei.me/data/brename/brename_linux_386.tar.gz))                            |[![Github Releases (by Asset)](https://img.shields.io/github/downloads/shenwei356/brename/latest/brename_linux_386.tar.gz.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_linux_386.tar.gz)
+Linux  |**64-bit**|[**brename_linux_amd64.tar.gz**](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_linux_amd64.tar.gz), ([mirror](http://app.shenwei.me/data/brename/brename_linux_amd64.tar.gz))                  |[![Github Releases (by Asset)](https://img.shields.io/github/downloads/shenwei356/brename/latest/brename_linux_amd64.tar.gz.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_linux_amd64.tar.gz)
+OS X   |32-bit    |[brename_darwin_386.tar.gz](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_darwin_386.tar.gz), ([mirror](http://app.shenwei.me/data/brename/brename_darwin_386.tar.gz))                         |[![Github Releases (by Asset)](https://img.shields.io/github/downloads/shenwei356/brename/latest/brename_darwin_386.tar.gz.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_darwin_386.tar.gz)
+OS X   |**64-bit**|[**brename_darwin_amd64.tar.gz**](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_darwin_amd64.tar.gz), ([mirror](http://app.shenwei.me/data/brename/brename_darwin_amd64.tar.gz))               |[![Github Releases (by Asset)](https://img.shields.io/github/downloads/shenwei356/brename/latest/brename_darwin_amd64.tar.gz.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_darwin_amd64.tar.gz)
+Windows|32-bit    |[brename_windows_386.exe.tar.gz](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_windows_386.exe.tar.gz), ([mirror](http://app.shenwei.me/data/brename/brename_windows_386.exe.tar.gz))          |[![Github Releases (by Asset)](https://img.shields.io/github/downloads/shenwei356/brename/latest/brename_windows_386.exe.tar.gz.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_windows_386.exe.tar.gz)
+Windows|**64-bit**|[**brename_windows_amd64.exe.tar.gz**](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_windows_amd64.exe.tar.gz), ([mirror](http://app.shenwei.me/data/brename/brename_windows_amd64.exe.tar.gz))|[![Github Releases (by Asset)](https://img.shields.io/github/downloads/shenwei356/brename/latest/brename_windows_amd64.exe.tar.gz.svg?maxAge=3600)](https://github.com/shenwei356/brename/releases/download/v0.2.0/brename_windows_amd64.exe.tar.gz)
+
+
+Just [download](https://github.com/shenwei356/brename/releases) compressed
+executable file of your operating system,
+and decompress it with `tar -zxvf *.tar.gz` command or other tools.
+And then:
+
+1. **For Linux-like systems**
+    1. If you have root privilege simply copy it to `/usr/local/bin`:
+
+            sudo cp brename /usr/local/bin/
+
+    1. Or add the current directory of the executable file to environment variable
+    `PATH`:
+
+            echo export PATH=\$PATH:\"$(pwd)\" >> ~/.bashrc
+            source ~/.bashrc
+
+
+1. **For windows**, just copy `brename.exe` to `C:\WINDOWS\system32`.
+
+#### Method 2: For Go developer
+
+    go get -u github.com/shenwei356/brename/
+
+
+## Usage
+
+```
+brename -- a cross-platform command-line tool for batch renaming files/directories
+
+Version: 0.2.0
+
+Author: Wei Shen <shenwei356@gmail.com>
+
+Homepage: https://github.com/shenwei356/brename
+
+Usage:
+  brename [flags]
+
+Examples:
+  1. renaming all .jpeg files to .jpg in all subdirs
+      brename -p "\.jpeg" -r ".jpg" -R   dir
+  2. doubling "a"
+      brename -p "(a)" -r "\$1\$1"  or brename -p "(a)" -r '$1$1'
+  3. even renaming directory
+      brename -p ":" -r "-" -R -D   pdf-dirs
+  4. dry run and showing potential dangerous operation
+      brename -p "abc" -d -v 2
+
+  More examples: https://github.com/shenwei356/brename
+
+Flags:
+  -d, --dry-run              print rename operations but do not run
+  -i, --ignore-case          ignore case
+  -D, --including-dir        rename directories
+  -p, --pattern string       search pattern (regular expression)
+  -R, --recursive            rename recursively
+  -r, --replacement string   replacement
+  -v, --verbose int          verbose level (0 for all, 1 for warning and error, 2 for only error)
+  -V, --version              print version information and check for update
+
+```
+
+
+## Examples
+
+
+## Contact
+
+[Create an issue](https://github.com/shenwei356/brename/issues) to report bugs,
+propose new functions or ask for help.
+
+## License
 
 [MIT License](https://github.com/shenwei356/brename/blob/master/LICENSE)
--- brename.go
+++ brename.go
@@ -1,167 +1,392 @@
-// Copyright 2015 Wei Shen (shenwei356@gmail.com). All rights reserved.
-// Use of this source code is governed by a MIT-license
-// that can be found in the LICENSE file.
+// Copyright © 2017 Wei Shen <shenwei356@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-// Recursively batch rename files and directories by regular expression.
 package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/op/go-logging"
+	"github.com/spf13/cobra"
 )
 
-var (
-	path string // path
-	src  string // source regular expression
-	repl string // replacement
-	R    bool   // recursive
-	D    bool   // Rename directories
-	dryrun bool // dry run
-)
+var log *logging.Logger
 
-func init() {
-	flag.StringVar(&src, "s", "", "Search pattern (regular expression)")
-	flag.StringVar(&repl, "r", "", "Replacement")
-	flag.BoolVar(&R, "R", false, "Recursively rename")
-	flag.BoolVar(&D, "D", false, "Rename directories")
-	flag.BoolVar(&dryrun, "d", false, "Dry run")
+var VERSION = "0.2.0"
+var APP = "brename"
 
-	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, "\nbrename\n  Recursively batch rename files and directories by regular expression.")
-		fmt.Fprintf(os.Stderr, "\nUsage: %s -s <regexp> -r <replacement> [-R] [-D] [path...]\n\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, "Options:")
-		flag.PrintDefaults()
-		fmt.Fprint(os.Stderr, `
-Example:
-  1. a.jpeg -> a.jpg
-     brename -s '\.jpeg$' -r '.jpg'
-  2. ab.png -> abab.png
-     brename -s '([ab]+)' -r '$1$1'
-`)
-		fmt.Fprintln(os.Stderr, "\n  Site: https://github.com/shenwei356/brename")
-		fmt.Fprintln(os.Stderr, "Author: Wei Shen (shenwei356@gmail.com)\n")
+// Options is the struct containing all global options
+type Options struct {
+	Verbose int
+	Version bool
+	DryRun  bool
+
+	Pattern      string
+	PatternRe    *regexp.Regexp
+	Replacement  string
+	Recursive    bool
+	IncludingDir bool
+	IgnoreCase   bool
+}
+
+func getOptions(cmd *cobra.Command) *Options {
+	version := getFlagBool(cmd, "version")
+	if version{
+		checkVersion()
+		return &Options{Version: version}
 	}
 
-	flag.Parse()
-	if src == "" {
-		fmt.Fprintln(os.Stderr, "option -s needed")
+	pattern := getFlagString(cmd, "pattern")
+	if pattern == "" {
+		log.Errorf("flag -p/--pattern needed")
 		os.Exit(1)
 	}
+	p := pattern
+	if getFlagBool(cmd, "ignore-case") {
+		p = "(?i)" + p
+	}
+	re, err := regexp.Compile(p)
+	if err != nil {
+		log.Errorf("Illegal regular expression: %s", pattern)
+		os.Exit(1)
+	}
+
+	return &Options{
+		Verbose: getFlagNonNegativeInt(cmd, "verbose"),
+		Version: version,
+		DryRun:  getFlagBool(cmd, "dry-run"),
+
+		Pattern:      pattern,
+		PatternRe:    re,
+		Replacement:  getFlagString(cmd, "replacement"),
+		Recursive:    getFlagBool(cmd, "recursive"),
+		IncludingDir: getFlagBool(cmd, "including-dir"),
+	}
+}
+
+func init() {
+	logFormat := logging.MustStringFormatter(`%{color}[%{level:.4s}]%{color:reset} %{message}`)
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, logFormat)
+	logging.SetBackend(backendFormatter)
+	log = logging.MustGetLogger(APP)
+
+	RootCmd.Flags().IntP("verbose", "v", 0, "verbose level (0 for all, 1 for warning and error, 2 for only error)")
+	RootCmd.Flags().BoolP("version", "V", false, "print version information and check for update")
+	RootCmd.Flags().BoolP("dry-run", "d", false, "print rename operations but do not run")
+
+	RootCmd.Flags().StringP("pattern", "p", "", "search pattern (regular expression)")
+	RootCmd.Flags().StringP("replacement", "r", "", "replacement")
+	RootCmd.Flags().BoolP("recursive", "R", false, "rename recursively")
+	RootCmd.Flags().BoolP("including-dir", "D", false, "rename directories")
+	RootCmd.Flags().BoolP("ignore-case", "i", false, "ignore case")
+
+	RootCmd.Example = `  1. renaming all .jpeg files to .jpg in all subdirs
+      brename -p "\.jpeg" -r ".jpg" -R   dir
+  2. doubling "a"
+      brename -p "(a)" -r "\$1\$1"  or brename -p "(a)" -r '$1$1'
+  3. even renaming directory
+      brename -p ":" -r "-" -R -D   pdf-dirs
+  4. dry run and showing potential dangerous operation
+      brename -p "abc" -d -v 2
+
+  More examples: https://github.com/shenwei356/brename`
+
+	RootCmd.SetUsageTemplate(`Usage:{{if .Runnable}}
+  {{if .HasAvailableFlags}}{{appendIfNotPresent .UseLine "[flags]"}}{{else}}{{.UseLine}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+  {{ .CommandPath}} [command]{{end}} {{if gt .Aliases 0}}
+
+Aliases:
+  {{.NameAndAliases}}
+{{end}}{{if .HasExample}}
+
+Examples:
+{{ .Example }}{{end}}{{ if .HasAvailableSubCommands}}
+
+Available Commands:{{range .Commands}}{{if .IsAvailableCommand}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimRightSpace}}{{end}}{{ if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimRightSpace}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsHelpCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{ if .HasAvailableSubCommands }}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`)
 }
 
 func main() {
-	re, err := regexp.Compile(src)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "[Error] invalid regular expression!")
-		os.Exit(1)
-	}
-
-	var paths []string
-	if len(flag.Args()) == 0 {
-		paths = []string{"./"}
-	} else {
-		paths = flag.Args()
-	}
-	for _, path := range paths {
-		fmt.Fprintf(os.Stderr, "%s:\n", path)
-		n, err := BatchRename(path, re, repl, R, D)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		fmt.Fprintf(os.Stderr, "%d files renamed.\n\n", n)
+	if err := RootCmd.Execute(); err != nil {
+		log.Error(err)
+		os.Exit(-1)
 	}
 }
 
-func BatchRename(path string, re *regexp.Regexp, repl string, recursive bool, D bool) (uint, error) {
-	var n uint = 0
+func checkError(err error) {
+	if err != nil {
+		log.Error(err)
+		os.Exit(-1)
+	}
+}
 
+func getFileList(args []string) []string {
+	files := []string{}
+	if len(args) == 0 {
+		files = append(files, "./")
+	} else {
+		for _, file := range files {
+			if file == "./" {
+				continue
+			}
+			if _, err := os.Stat(file); os.IsNotExist(err) {
+				checkError(err)
+			}
+		}
+		files = args
+	}
+	return files
+}
+
+func getFlagBool(cmd *cobra.Command, flag string) bool {
+	value, err := cmd.Flags().GetBool(flag)
+	checkError(err)
+	return value
+}
+
+func getFlagString(cmd *cobra.Command, flag string) string {
+	value, err := cmd.Flags().GetString(flag)
+	checkError(err)
+	return value
+}
+
+func getFlagNonNegativeInt(cmd *cobra.Command, flag string) int {
+	value, err := cmd.Flags().GetInt(flag)
+	checkError(err)
+	if value < 0 {
+		checkError(fmt.Errorf("value of flag --%s should be greater than or equal to 0", flag))
+	}
+	return value
+}
+
+func checkVersion() {
+	app := APP
+	fmt.Printf("%s v%s\n", app, VERSION)
+	fmt.Println("\nChecking new version...")
+
+	resp, err := http.Get(fmt.Sprintf("https://github.com/shenwei356/%s/releases/latest", app))
+	if err != nil {
+		checkError(fmt.Errorf("Network error"))
+	}
+	items := strings.Split(resp.Request.URL.String(), "/")
+	version := ""
+	if items[len(items)-1] == "" {
+		version = items[len(items)-2]
+	} else {
+		version = items[len(items)-1]
+	}
+	if version == "v"+VERSION {
+		fmt.Printf("You are using the latest version of %s\n", app)
+	} else {
+		fmt.Printf("New version available: %s %s at %s\n", app, version, resp.Request.URL.String())
+	}
+}
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   APP,
+	Short: "a cross-platform command-line tool for batch renaming files/directories",
+	Long: fmt.Sprintf(`
+brename -- a cross-platform command-line tool for batch renaming files/directories
+
+Version: %s
+
+Author: Wei Shen <shenwei356@gmail.com>
+
+Homepage: https://github.com/shenwei356/brename
+
+`, VERSION),
+	Run: func(cmd *cobra.Command, args []string) {
+		// var err error
+		opt := getOptions(cmd)
+
+		if opt.Version {
+			return
+		}
+
+		ops := make([]operation, 0, 100)
+		var err error
+
+		for _, path := range getFileList(args) {
+			ops, err = walk(opt, ops, path)
+			checkError(err)
+		}
+
+		var hasErr bool
+		for _, op := range ops {
+			if int(op.code) >= opt.Verbose {
+				log.Infof("TODO: %s\n", op)
+			}
+
+			if int(op.code) > 1 {
+				hasErr = true
+			}
+		}
+
+		if opt.DryRun {
+			return
+		}
+
+		if hasErr {
+			checkError(errors.New("potential errors will occur, please check!!!"))
+
+		}
+
+		for _, op := range ops {
+			err := os.Rename(op.source, op.target)
+			if err != nil {
+				log.Errorf("fail to rename: %s -> %s", op.source, op.target)
+				os.Exit(-1)
+			}
+			if opt.Verbose >= 0 {
+				log.Infof("renamed: %s -> %s", op.source, op.target)
+			}
+		}
+	},
+}
+
+type code int
+
+const (
+	codeOK code = iota
+	codeNoChange
+	codeExisted
+	codeMissingTarget
+)
+
+var yellow = color.New(color.FgYellow).SprintFunc()
+var red = color.New(color.FgRed).SprintFunc()
+
+func (c code) String() string {
+	switch c {
+	case codeOK:
+		return "ok"
+	case codeNoChange:
+		return yellow("not changed")
+	case codeExisted:
+		return red("new path existed")
+	case codeMissingTarget:
+		return red("missing target")
+	}
+	return "undefined code"
+}
+
+type operation struct {
+	source, target string
+	code           code
+}
+
+func (op operation) String() string {
+	return fmt.Sprintf("%s -> %s: %s", op.source, op.target, op.code)
+}
+
+func check(opt *Options, path string) (bool, operation) {
+	dir, filename := filepath.Split(path)
+
+	if !opt.PatternRe.MatchString(filename) {
+		return false, operation{}
+	}
+
+	filename2 := opt.PatternRe.ReplaceAllString(filename, opt.Replacement)
+	if filename2 == "" {
+		return true, operation{path, filename2, codeMissingTarget}
+	}
+
+	if filename2 == filename {
+		return true, operation{path, filepath.Join(dir, filename2), codeNoChange}
+	}
+
+	target := filepath.Join(dir, filename2)
+	if _, err := os.Stat(target); err == nil {
+		return true, operation{path, target, codeExisted}
+	}
+
+	return true, operation{path, filepath.Join(dir, filename2), codeOK}
+}
+
+func walk(opt *Options, ops []operation, path string) ([]operation, error) {
 	_, err := ioutil.ReadFile(path)
 	// it's a file
 	if err == nil {
-		n, err = Rename(path, re, repl)
-		if err != nil {
-			return 0, err
+		if ok, op := check(opt, path); ok {
+			ops = append(ops, op)
 		}
-		return n, nil
+		return ops, nil
 	}
 
 	// it's a directory
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		if os.IsNotExist(err) {
-			return 0, errors.New("[Error] Path not exist: " + path)
-		}
-		return 0, errors.New("[Error] Path read error: " + path)
+		return ops, err
 	}
 
 	var filename string
 	for _, file := range files {
 		filename = file.Name()
-		if filename == "." || filename == ".." {
+		if filename[0] == '.' {
 			continue
 		}
 
 		fileFullPath := filepath.Join(path, filename)
 		// sub directory
 		if file.IsDir() {
-			if recursive {
-				num, err := BatchRename(fileFullPath, re, repl, recursive, D)
+			if opt.Recursive {
+				var err error
+				ops, err = walk(opt, ops, fileFullPath)
 				if err != nil {
-					return n, err
+					return ops, err
 				}
-				n += num
 			}
 			// Rename directories
-			if D {
-				num, err := Rename(fileFullPath, re, repl)
-				if err != nil {
-					return n, err
+			if opt.IncludingDir {
+				if ok, op := check(opt, fileFullPath); ok {
+					ops = append(ops, op)
 				}
-				n += num
 			}
 		} else {
-			num, err := Rename(fileFullPath, re, repl)
-			if err != nil {
-				return n, err
+			if ok, op := check(opt, fileFullPath); ok {
+				ops = append(ops, op)
 			}
-			n += num
 		}
 	}
-	return n, nil
-}
 
-func Rename(path string, re *regexp.Regexp, repl string) (uint, error) {
-	dir, filename := filepath.Split(path)
-
-	// not matched
-	if !re.Match([]byte(filename)) {
-		return 0, nil
-	}
-
-	filename2 := re.ReplaceAllString(filename, repl)
-	// not changed
-	if filename2 == filename {
-		return 0, nil
-	}
-
-	// duplicated files
-	// in windows, rename a file to another existed file will cause err.
-	// however, it will succeed in Linux.
-	if _, err := os.Stat(filename2); err == nil {
-		return 0, errors.New("[Error] Rename file error: " + filename + " -> " + filename2 + " (" + filename2 + " already existed!)")
-	}
-
-	// rename
-	err := os.Rename(path, filepath.Join(dir, filename2))
-	if err != nil {
-		return 0, errors.New(fmt.Sprintf("[Error] %v", err))
-	}
-
-	return 1, nil
+	return ops, nil
 }
